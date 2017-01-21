0dba1a21d0ab9b1483fdca3b38db6d0b467720d9
humbamp123 <humb.amp123@gmail.com>
2017-01-21 09:20:40 -0800

    Added singleton to instance in WaveDetector

---
+++ HeadphoneGoldfish/Assets/MusicTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTest : MonoBehaviour {
+    public float currspeed;
+    FMOD.Studio.EventInstance mx;
+    FMOD.Studio.ParameterInstance mxSpeed;
+
+	// Use this for initialization
+	void Start () {
+        //var studio = new FMODUnity.RuntimeManager();
+        string x = "event:/MX";
+        //studio.play
+        // FMODUnity.RuntimeManager.PlayOneShot(x);
+        
+        mx = FMODUnity.RuntimeManager.CreateInstance(x);
+        mx.getParameter("Speed", out mxSpeed);
+        mx.start();
+        mxSpeed.setValue(1F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var res = mx.setParameterValue("Speed", currspeed);
+        float x;
+        mxSpeed.getValue(out x);
+        //Debug.Log(x);		
+	}
+}
