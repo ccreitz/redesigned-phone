d0eaa0ca25585adeb7bced8d78f42e256eee0a8e
U-NIMBLESTORAGE\msubkhankulov <msubkhankulov@tp-marats.nimblestorage.com>
2017-01-20 21:52:51 -0800

    Added tunable speed to segment spawning code.

--- HeadphoneGoldfish/Assets/segment.cs
+++ HeadphoneGoldfish/Assets/segment.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class segment : MonoBehaviour
 {
+    public static float speed;
     public float clip_threshold;
+    private bool is_out_of_bounds;
     // Use this for initialization
     void Start()
     {
-
+        is_out_of_bounds = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
+        transform.position += new Vector3(gameObject.GetComponentInParent<spawner>().GetScrollSpeed(), 0, 0) * Time.deltaTime;
 
         if (transform.position.x < clip_threshold)
         {
             Destroy(gameObject);
+            is_out_of_bounds = true;
         }
+    }
+
+    public bool IsOutOfBounds()
+    {
+        return is_out_of_bounds;
     }
 }
