e1c3a17f2da3685fa6d2aface7534f7e0df39934
humbamp123 <humb.amp123@gmail.com>
2017-01-21 11:13:04 -0800

    Added a sprite texture to spawning objects

--- HeadphoneGoldfish/Assets/spawner.cs
+++ HeadphoneGoldfish/Assets/spawner.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class spawner : MonoBehaviour {
     public Transform segment;
     public float time_between_spawn;
-    public float scroll_speed;
+    public float scroll_speed_default;
     public List<Transform> segments;
+    private float scroll_speed;
 
     private float last_spawn_time;
 
 	// Use this for initialization
 	void Start () {
+        scroll_speed = scroll_speed_default;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        scroll_speed = scroll_speed_default - WaveDetector.Instance.Speed;
         float elapsed = (Time.time - last_spawn_time);
         if (elapsed > time_between_spawn/-scroll_speed) {
             spawn();
         }
+        
     }
 
     void spawn()
