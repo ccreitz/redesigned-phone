ab945059ffe2559d9170a028fdb0cf8a8abf9384
humbamp123 <humb.amp123@gmail.com>
2017-01-21 09:55:40 -0800

    Fixed music controller

---
+++ HeadphoneGoldfish/Assets/MusicController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+    FMOD.Studio.EventInstance mx;
+    private int lastSpeed = 0;
+    // Use this for initialization
+    void Start()
+    {
+        //var studio = new FMODUnity.RuntimeManager();
+        string x = "event:/MX";
+
+        mx = FMODUnity.RuntimeManager.CreateInstance(x);
+        mx.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lastSpeed != WaveDetector.Instance.Speed)
+        {
+            Debug.Log("Changing from " + lastSpeed + " to " + WaveDetector.Instance.Speed);
+            lastSpeed = WaveDetector.Instance.Speed;
+            mx.setParameterValue("Speed", WaveDetector.Instance.Speed);
+
+        }
+        	
+    }
+}
