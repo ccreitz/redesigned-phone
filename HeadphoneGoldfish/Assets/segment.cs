d874f0217dabaf87f7fe28a2c8883b5799a7daac
U-NIMBLESTORAGE\msubkhankulov <msubkhankulov@tp-marats.nimblestorage.com>
2017-01-20 20:40:58 -0800

    Endless scroll

---
+++ HeadphoneGoldfish/Assets/segment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class segment : MonoBehaviour
+{
+    public float clip_threshold;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
+
+        if (transform.position.x < clip_threshold)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ HeadphoneGoldfish/Assets/spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+    public Transform segment;
+    public float time_between_spawn;
+    private float last_spawn_time;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Time.time - last_spawn_time) > time_between_spawn) {
+            spawn();
+        }
+	}
+
+    void spawn()
+    {
+        GameObject.Instantiate(segment);
+        last_spawn_time = Time.time;
+    }
+}
