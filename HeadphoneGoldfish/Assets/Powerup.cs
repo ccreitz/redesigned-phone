283a5b9ac8998f9a7f5a76c45e95ffffda3aaea4
U-NIMBLESTORAGE\msubkhankulov <msubkhankulov@tp-marats.nimblestorage.com>
2017-01-22 11:36:08 -0800

    changed powerup sprite

--- HeadphoneGoldfish/Assets/Powerup.cs
+++ HeadphoneGoldfish/Assets/Powerup.cs
@@ -8,15 +8,13 @@ public class Powerup : MonoBehaviour
     private SpriteRenderer sRend;
     private MusicController musControl;
 
-	// Use this for initialization
-	void Start () {
-        sRend = GetComponent<SpriteRenderer>();
-        musControl = GetComponentInParent<MusicController>();
+    // Use this for initialization
+    void Start()
+    {
     }
-	
-	// Update is called once per frame
-	void Update () {
-        float factor = 1.0f - (Mathf.Max(Time.time - musControl.lastBeatTime, 0) / beatDuration);
-        sRend.color = new Color(factor, factor, factor);
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
 }
