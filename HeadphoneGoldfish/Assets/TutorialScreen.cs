b13c746a1ec7ed2f5b79d12601d6d5fa3446bc2e
Marat Subkhankulov <msubkhankulov@nimblestorage.com>
2017-07-02 18:38:59 -0700

    Press any key to exit tutorial

---
+++ HeadphoneGoldfish/Assets/TutorialScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			GameObject.Find ("MenuManager").GetComponent<MenuManager> ().start_Tutorial ();
+		}
+	}
+}
