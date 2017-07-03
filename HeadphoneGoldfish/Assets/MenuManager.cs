dbf098350b97062edddd92d083a1ba58e2b3973a
humbamp123 <humb.amp123@gmail.com>
2017-07-02 18:29:59 -0700

    Add tutorial screen

--- HeadphoneGoldfish/Assets/MenuManager.cs
+++ HeadphoneGoldfish/Assets/MenuManager.cs
@@ -5,10 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
     public UnityEngine.UI.Text highScore;
+    public GameObject tutorial_screen;
+    public GameObject hide_canvas;
+    public bool first_game;
 
     private void Start()
     {
         highScore.text = PlayerPrefs.GetInt("score", 0).ToString();
+    }
+
+    public void start_Tutorial()
+    {
+        if (first_game)
+        {
+            hide_canvas.SetActive(false);
+            tutorial_screen.SetActive(true);
+            first_game = false;
+        }
+        else
+        {
+            StartGame();
+        }
     }
 
     public void StartGame()
